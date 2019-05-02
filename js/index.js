8401c360847c30dc656f822e3a98e325afa7a64a
Matthew McIntyre <mattymac2@gmail.com>
2019-05-03 00:40:25 +0100

    inital commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var weatherAPI = 'https://fcc-weather-api.glitch.me/api/current?';
+var weatherAPI = "https://fcc-weather-api.glitch.me/api/current?";
 var currentTemp = 0;
 var weatherCode = 0;
 var umbrella;
@@ -8,86 +8,82 @@ var temp = document.getElementById("myTemp");
 var answer = document.getElementById("answer");
 var myCurrentWeather = document.getElementById("myCurrentWeather");
 
-if(navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(function(position){  
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-    
+
     weatherAPI += "lat=" + lat + "&lon=" + lon;
-  
-    fetch(weatherAPI)
-    .then(function(response){
-      if (response.ok){
-        response.json().then(function(data){
-          
+
+    fetch(weatherAPI).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
           myLocation.innerHTML = data["name"];
-          
+
           myCurrentWeather.innerHTML = data["weather"][0]["description"];
-       
+
           currentTemp = data["main"]["temp"];
-          
-          
+
           weatherCode = data["weather"][0]["id"];
-          
+
           toCelsius();
-          chooseIcon();    
-        })
+          chooseIcon();
+        });
       }
-    })
-  })  
+    });
+  });
 }
 
-function toCelsius(){
-  myTemp.setAttribute( "onClick", "javascript: toFahreinheit();" );
-  myTemp.innerHTML = currentTemp + String.fromCharCode(176) + "C";  
+function toCelsius() {
+  myTemp.setAttribute("onClick", "javascript: toFahreinheit();");
+  myTemp.innerHTML = currentTemp + String.fromCharCode(176) + "C";
 }
 
-function toFahreinheit(){
-  myTemp.setAttribute( "onClick", "javascript: toCelsius();" );
-  myTemp.innerHTML = Math.floor((currentTemp * 1.8) + 32) + String.fromCharCode(176) + "F"; 
+function toFahreinheit() {
+  myTemp.setAttribute("onClick", "javascript: toCelsius();");
+  myTemp.innerHTML =
+    Math.floor(currentTemp * 1.8 + 32) + String.fromCharCode(176) + "F";
 }
 
-function chooseIcon(){
+function chooseIcon() {
   //Thunderstorm
-  if(weatherCode >= 200 && weatherCode <= 232){
+  if (weatherCode >= 200 && weatherCode <= 232) {
     answer.innerHTML = "Yes!";
-  document.getElementById("thunder-storm").classList.remove("hidden");
+    document.getElementById("thunder-storm").classList.remove("hidden");
   }
   //Drizzle
-  else if(weatherCode >= 300 && weatherCode <= 321){
+  else if (weatherCode >= 300 && weatherCode <= 321) {
     answer.innerHTML = "Yes!";
-  document.getElementById("sun-shower").classList.remove("hidden");
+    document.getElementById("sun-shower").classList.remove("hidden");
   }
   //Rain
-  else if(weatherCode >= 500 && weatherCode <= 531){
-
+  else if (weatherCode >= 500 && weatherCode <= 531) {
     answer.innerHTML = "Yes!";
     document.getElementById("rainy").classList.remove("hidden");
   }
   //Snow
-  else if(weatherCode >= 600 && weatherCode <= 622){
+  else if (weatherCode >= 600 && weatherCode <= 622) {
     myIcon.src = "this";
     answer.innerHTML = "Yes!";
     document.getElementById("flurries").classList.remove("hidden");
   }
   //Atmosphere
-  else if(weatherCode >= 700 && weatherCode <= 781){
+  else if (weatherCode >= 700 && weatherCode <= 781) {
     myIcon.src = "this";
     answer.innerHTML = "No!";
     document.getElementById("sunny").classList.remove("hidden");
   }
   //Clear
-  else if(weatherCode == 800){
+  else if (weatherCode == 800) {
     answer.innerHTML = "No!";
-  document.getElementById("sunny").classList.remove("hidden");
+    document.getElementById("sunny").classList.remove("hidden");
   }
   //Clouds
-  else if(weatherCode >= 801 && weatherCode <= 804){
+  else if (weatherCode >= 801 && weatherCode <= 804) {
     answer.innerHTML = "No!";
-  document.getElementById("cloudy").classList.remove("hidden");
+    document.getElementById("cloudy").classList.remove("hidden");
   }
 }
-
 
 //todo adjust the icon choosing method if statements to be more bullet proof and specific...
 
